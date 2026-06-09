20865d78e4f302a382362ae7dcdbe4db3ee58a04
Matheus00DEV <matheus2009sousa0@gmail.com>
2026-06-09 13:58:15 -0300

    Add files via upload

--- backend/routes/motoristaRoutes.js
+++ backend/routes/motoristaRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const motoristaController = require('../controllers/motoristaController');
-const { exigirAutenticacao, exigirPermissaoCadastro } = require('../middleware/authMiddleware');
+const { exigirAutenticacao, exigirOperacional, exigirPermissaoCadastro } = require('../middleware/authMiddleware');
 
 router.use(exigirAutenticacao);
-router.get('/', motoristaController.getMotoristas);
+router.get('/', exigirOperacional, motoristaController.getMotoristas);
 router.post('/', exigirPermissaoCadastro, motoristaController.criarMotorista);
 router.put('/:cpf', exigirPermissaoCadastro, motoristaController.atualizarMotorista);
 router.delete('/:cpf', exigirPermissaoCadastro, motoristaController.excluirMotorista);
